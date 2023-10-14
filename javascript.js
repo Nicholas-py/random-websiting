7060029204ad7f742092a1138c9bde5293631f9d
Nicholas-py <74473358+Nicholas-py@users.noreply.github.com>
2023-10-13 22:40:07 -0400

    Make a new button

--- javascript.js
+++ javascript.js
@@ -1,4 +1,10 @@
-
 function celebrate() {
   document.getElementById("hiddenMessage").style.display = 'block';
+  document.getElementById('agreeButton').innerHTML = 'Agree';
+
+}
+
+function moreButtoning() {
+  document.getElementById("hiddenMessage").style.display = 'none';
+  document.getElementById('agreeButton').innerHTML = 'You don\'t need to do it again';
 }
