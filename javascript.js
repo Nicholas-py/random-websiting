76966e7f0d2152b7230b5b43d187933a8b892251
Nicholas-py <74473358+Nicholas-py@users.noreply.github.com>
2023-10-15 20:01:45 -0400

    Add a live adding text

--- javascript.js
+++ javascript.js
@@ -1,10 +1,29 @@
+
 function celebrate() {
   document.getElementById("hiddenMessage").style.display = 'block';
   document.getElementById('agreeButton').innerHTML = 'Agree';
 
 }
 
+function textholderloaded() {
+  var globaltext = localStorage.getItem('gobacktext');
+  document.getElementById('showuptext').innerHTML = globaltext;
+  }
+
+
 function moreButtoning() {
   document.getElementById("hiddenMessage").style.display = 'none';
   document.getElementById('agreeButton').innerHTML = 'You don\'t need to do it again';
+}
+
+function uselessSubmitted() {
+  document.getElementById('goback').style.display = 'block'
+  var text = document.getElementById('Textbox').value;
+  oldtext = localStorage.getItem('gobacktext');
+  if (oldtext == null) {
+    localStorage.setItem('gobacktext', text)
+  } else {
+    localStorage.setItem('gobacktext',oldtext + text);
+  }
+  
 }
